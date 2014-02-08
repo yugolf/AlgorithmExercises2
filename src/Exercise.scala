ae00cf8fb62e2d3074b763efed69402e0353386b
Yugo Maede <yugo.maede@gmail.com>
2014-02-09 00:04:20 +0900

    delete old logic

--- src/Exercise.scala
+++ src/Exercise.scala
@@ -1,9 +1,9 @@
 object Exercise {
   def main(args: Array[String]): Unit = {
-    println(isSubstring1("abcdefgabe","gab"))
+    println(isSubstring("abcdefgabe","gab"))
   }
 
-  def isSubstring1 (full:String, part:String) = {
+  def isSubstring (full:String, part:String) = {
     def check(full: String, fullPos: Int, part: String, partPos: Int):Int ={
       if(fullPos == full.length || partPos == part.length) partPos
       else if(full(fullPos) == part(partPos)) check(full, fullPos + 1, part, partPos + 1)
@@ -12,27 +12,4 @@ object Exercise {
 
     check(full, 0, part, 0) == part.length
   }
-
-  def isSubstring2(s1: String, s2: String):Boolean = {
-
-    val s2l = s2.length -1
-    println("s2l:" + s2l)
-
-    var s2count = 0
-
-      for(j <- 0 until s1.length){
-        if(s2(s2count) == s1(j)) {
-          println("part:" + s2count + ":" + s2(s2count)+ ",full:"+ j +":" + s1(j)+ "s2count:" + s2count)
-          if(s2count == s2l) return true
-          s2count = s2count + 1
-        } else {
-          s2count = 0
-        }
-      }
-      s2count = s2count + 1
-      println(s2(s2count) + "s2count" + s2count)
-    false
-  }
-
-
 }
